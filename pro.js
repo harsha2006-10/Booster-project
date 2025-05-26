6580e4c7766e29e6d78ba2d7de418e822e713f05
harsha2006-10 <hvardhann03@gmail.com>
2025-05-26 21:41:42 +0530

    js file also added

---
+++ pro.js
@@ -0,0 +1,13 @@
+ifunction changeNews() {
+        const region = document.getElementById('region').value;
+        alert(`News updated for region: ${region}`);
+}
+function ok()
+{
+    alert("Signed up successfully!");
+    window.location.href = ".vscode/project.html"; 
+}
+function okk() {
+        alert("login successfully!");
+    window.location.href = ".vscode/project.html";
+}
